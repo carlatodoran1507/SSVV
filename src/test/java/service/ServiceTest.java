1d38e86fde0eddba3db848204c657b2bddbdd018
Ianis_Teja <65509255+IanisT227@users.noreply.github.com>
2023-03-24 19:40:50 +0200

    Added test file cu test in ea

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,7 +1,15 @@
 package service;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, 17);
+    }
 
 }
