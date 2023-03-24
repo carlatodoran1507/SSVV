542bd02b58e0e46537347d5834bf5049150266e3
Carla TODORAN-PESCARUS <ctodoran@bitdefender.com>
2023-03-24 19:09:11 +0200

    Added demo test

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,7 +1,14 @@
 package service;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
 
+    @Test
+    void addStudent() {
+        assertTrue(true);
+    }
 }
+
