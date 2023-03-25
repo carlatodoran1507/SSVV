80388a98ea96ce33c8b21a1dc93851d0e4a129c2
Carla TODORAN-PESCARUS <carlatodoran1507@gmail.com>
2023-03-25 14:59:31 +0200

    Added mockito

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,15 +1,73 @@
 package service;
 
-import org.junit.Assert;
+import domain.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-class ServiceTest {
+class ServiceTest  {
 
-    @Test
-    public void testAdd() {
-        Assert.assertEquals(17, 17);
+    @Mock
+    private StudentXMLRepo studentXMLRepository;
+
+    @Mock
+    private TemaXMLRepo temaXMLRepository;
+
+    @Mock
+    private NotaXMLRepo notaXMLRepository;
+
+    @InjectMocks
+    private Service victim;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        victim = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
     }
 
+    @Test
+    void shouldAddStudent() {
+        // given
+        var id = "id";
+        var nume = "nume";
+        var grupa = 1;
+        Student student = new Student(id, nume, grupa);
+
+        // when
+        when(studentXMLRepository.save(any())).thenReturn(student);
+
+        // actual
+        int result = victim.saveStudent(id, nume, grupa);
+
+        assertEquals(0, result);
+        verify(studentXMLRepository).save(student);
+    }
+
+    @Test
+    void shouldFailToAddStudent() {
+        // given
+        var id = "id";
+        var nume = "nume";
+        var grupa = 1;
+        Student student = new Student(id, nume, grupa);
+
+        // when
+        when(studentXMLRepository.save(any())).thenReturn(null);
+
+        // actual
+        int result = victim.saveStudent(id, nume, grupa);
+
+        assertEquals(1, result);
+        verify(studentXMLRepository).save(student);
+    }
 }
